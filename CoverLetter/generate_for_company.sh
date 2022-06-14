0db381831988cd2bccce9db1d7f2c4f37ea4b071
Osama Iqbal <iqbal.osama@icloud.com>
2022-06-14 12:20:00 +0530

    feat: Add feature to dynamically generate websites

--- CoverLetter/generate_for_company.sh
+++ CoverLetter/generate_for_company.sh
@@ -9,6 +9,7 @@ CITY=$2
 COUNTRY=$3
 JOB_NAME=$4
 RECRUITER=$5
+WEBSITE=$6
 
 
 # Replace the stubs in the TeX file
@@ -16,6 +17,7 @@ sed -i "0,/CompanyName/s/CompanyName/$COMPANY_NAME/" coverletter.tex
 sed -i "0,/City/s/City/$CITY/" coverletter.tex
 sed -i "0,/Country/s/Country/$COUNTRY/" coverletter.tex
 sed -i "0,/XYZ/s/XYZ/$JOB_NAME/" coverletter.tex
+sed -i "0,/Website/s/Website/$WEBSITE/" coverletter.tex
 
 if [ ! -z "$RECRUITER" ]; then
   sed -i "0,/Recruiter/s/Recruiter/$RECRUITER/" coverletter.tex
@@ -29,6 +31,7 @@ sed -i "0,/$COMPANY_NAME/s/$COMPANY_NAME/CompanyName/" coverletter.tex
 sed -i "0,/$CITY/s/$CITY/City/" coverletter.tex
 sed -i "0,/$COUNTRY/s/$COUNTRY/Country/" coverletter.tex
 sed -i "0,/$JOB_NAME/s/$JOB_NAME/XYZ/" coverletter.tex
+sed -i "0,/$WEBSITE/s/$WEBSITE/Website/" coverletter.tex
 
 if [ ! -z "$RECRUITER" ]; then
   sed -i "0,/$RECRUITER/s/$RECRUITER/Recruiter/" coverletter.tex
